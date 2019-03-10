54eb1178e8f98e59882960533adac9f12dfeb4ce
wert310 <310wert@gmail.com>
2019-03-10 17:11:40 +0100

    docker_init docker-compose exec

--- docker_init.sh
+++ docker_init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker exec -ti ctforge_postgres_1 createuser -U postgres -d ctforge
-docker exec -ti ctforge_ctforge_1 ctforge init
+docker-compose exec ctforge_postgres_1 createuser -U postgres -d ctforge
+docker-compose exec ctforge_ctforge_1 ctforge init
