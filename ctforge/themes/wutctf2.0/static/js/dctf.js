def802204a7f8817e46573fcb61300e59b967bc8
MrStorm <mrstorm.nop@gmail.com>
2020-03-09 10:26:15 +0100

    writeups table now is rendered with datatables

--- ctforge/themes/wutctf2.0/static/js/dctf.js
+++ ctforge/themes/wutctf2.0/static/js/dctf.js
@@ -19,5 +19,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 $(document).ready(function() {
-    $('.sortable').footable();
+    $('.sortable').DataTable({
+        "paging": false,
+        "dom": "ft"
+    });
 });
